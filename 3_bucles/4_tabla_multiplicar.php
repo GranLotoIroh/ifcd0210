b8dedd0bc908f7ff5b115e322682b4ea0af4dacf
GranLotoIroh <joseluiyanezmenendez@gmail.com>
2025-02-26 19:37:32 +0100

    Ejercicios 26/2

--- 3_bucles/4_tabla_multiplicar.php
+++ 3_bucles/4_tabla_multiplicar.php
@@ -1,5 +1,9 @@
 <?php
-echo "<table style='border: 1px solid black;'>";
+echo "<table style='border: 1px solid black; text-align: center;'>";
+echo"<tr>
+    <th>Operación</th>
+    <th>Resultado</th>
+</tr>";
 for ($i=0; $i <=10 ; $i++) { 
     echo"<tr>";
         echo"<td>$i*7</td>"; 
